d36c1e5fad84982ca9f1b46328682c72a0b38cf9
Diego Cuevas <ingdcuevas@gmail.com>
2025-07-16 21:45:45 -0500

    feat: create & home view's finished

--- sanity/lib/write-client.ts
+++ sanity/lib/write-client.ts
@@ -1,14 +1,15 @@
-import "server-only"
-import { createClient } from "next-sanity"
-import { apiVersion, dataset, projectId, token } from '../env'
+import "server-only";
+import { createClient } from "next-sanity";
+import { apiVersion, dataset, projectId, token } from "../env";
 
 export const writeClient = createClient({
-    apiVersion,
-    dataset, projectId, 
-    useCdn: false,
-    token
-})
+  apiVersion,
+  dataset,
+  projectId,
+  useCdn: false,
+  token,
+});
 
-if(!writeClient.config().token) {
-    throw new Error('Write token not found.')
+if (!writeClient.config().token) {
+  throw new Error("Write token not found.");
 }
